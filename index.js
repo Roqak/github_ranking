f6378a97acde1a6c74ff21e1401cf4749da74ae0
Roqak <roqak1@gmail.com>
2019-05-07 14:03:52 +0100

    .

--- index.js
+++ index.js
@@ -134,33 +134,7 @@ app.get('/',(req,res)=>{
 
 app.get('/dashboard',(req,res)=>{
     console.log(`Your code is: ${options.form.code}`)
-    let getAuthenticatedUser = {
-        method: 'GET',
-        uri: 'https://api.github.com/user',
-        form: {
-            allow_signup: true,
-            client_id: keys.client_id,
-            client_secret: keys.client_secret,
-            state: "kdkdkddldldlkdkfd",
-            scope: 'repo,public_repo',
-        },
-        headers: {
-            'User-Agent': 'Roqak',
-            'Accept': 'application/json'
-          }
-    };
-    getAuthenticatedUser.form.code=  req.cookies.code
-    getAuthenticatedUser.headers.Authorization = `token ${accesscode}`
-    rp(getAuthenticatedUser)
-    .then(result=>{
-        // res.send(result)
-        myFollowers.push({
-            username: result.login
-        })
-    })
-    .catch(err=>{
-        res.send(err)
-    })
+    
     rp(options)
     .then(result=>{
         // console.log(req.header)
@@ -211,7 +185,33 @@ app.get('/dashboard',(req,res)=>{
                     //     res.send(user_error)
                     // })
                 }
-                
+                let getAuthenticatedUser = {
+                    method: 'GET',
+                    uri: 'https://api.github.com/user',
+                    form: {
+                        allow_signup: true,
+                        client_id: keys.client_id,
+                        client_secret: keys.client_secret,
+                        state: "kdkdkddldldlkdkfd",
+                        scope: 'repo,public_repo',
+                    },
+                    headers: {
+                        'User-Agent': 'Roqak',
+                        'Accept': 'application/json'
+                      }
+                };
+                getAuthenticatedUser.form.code=  req.cookies.code
+                getAuthenticatedUser.headers.Authorization = `token ${accesscode}`
+                rp(getAuthenticatedUser)
+                .then(result=>{
+                    // res.send(result)
+                    myFollowers.push({
+                        username: result.login
+                    })
+                })
+                .catch(err=>{
+                    res.send(err)
+                })
                 // res.json(myFollowers)
                 // GET USER EVENTS
                 let options2 = {
