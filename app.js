2ed0d1a39d7b4942030eaaa6a163e6a5a51eccb5
Roqak <roqak1@gmail.com>
2019-05-08 17:17:33 +0100

    080

--- app.js
+++ app.js
@@ -24,6 +24,21 @@ let currentUser = {
     username: "",
     repo_url: ""
 }
+let options2 = {
+    method: 'GET',
+uri: 'https://api.github.com/search/commits?q=author:sainttobs&type=Commits',
+form: {
+q:"Roqak",
+type:"commit",
+// state: "kdkdkddldldlkdkfd",
+// code: req.cookies.code
+},
+headers: {
+'User-Agent': 'Roqak',
+'Accept': 'application/vnd.github.cloak-preview'
+// 'Accept': 'application/json'
+}
+};
 let options = {
     method: 'POST',
     uri: 'https://github.com/login/oauth/access_token',
@@ -39,12 +54,18 @@ let options = {
         'Accept': 'application/json'
       }
 };
-let sample = [
-    {
-        name:"a",
-        age: 10
-}
-]
+let getUsers = {
+    method: 'get',
+    uri: 'https://api.github.com/user',
+    form: {
+        // access_token: "",
+        state: "kdkdkddldldlkdkfd"
+    },
+    headers: {
+        'User-Agent': 'Roqak',
+        'Accept': 'application/json'
+      },
+};
 function modify(aa){
     aa[0].jj="kk"
     return aa
@@ -52,6 +73,16 @@ function modify(aa){
 let myFollowers = [];
 let finalUser = [];
 
+let getFollowers = {
+    method: 'get',
+    form: {
+        state: "kdkdkddldldlkdkfd"
+    },
+    headers: {
+        'User-Agent': 'Roqak',
+        'Accept': 'application/json'
+      },
+};
 app.get('/',(req,res)=>{
     res.redirect(`https://github.com/login/oauth/authorize?allow_signup=true&client_id=${keys.client_id}`);
     
@@ -71,22 +102,60 @@ app.get('/auth',(req,res)=>{
 app.get("/dashboard",(req,res)=>{
     myFollowers=[]
     console.log(`Your code is: ${options.form.code}`);
-     (async function(){
-        try{
-            let user = await rp(options)
+    async function main(){
+            try {
+                let user = await rp(options)
             accesscode = JSON.parse(user).access_token
-            // console.log(`Your result: ${accesscode}`)
             options.form.code=  req.cookies.code
             getUsers.headers.Authorization = `token ${accesscode}`
-            let getAllFollowers = await rp(getUsers)
-            getAllFollowers = JSON.parse(getAllFollowers)
-            res.send(getAllFollowers)
+            let getAuthUser = await rp(getUsers)
+            getAuthUser = JSON.parse(getAuthUser)
+            myFollowers.push({
+                username: getAuthUser.login
+            })
+            let username = getAuthUser.login;
+            
+            let followers_url = getAuthUser.url;
+            let authenticated_user= getAuthUser.login
+            getFollowers.headers.Authorization = `token ${accesscode}`
+            getFollowers.uri = `${followers_url}/following`
+            let allMyFollowers = await rp(getFollowers)
+            let userData = JSON.parse(allMyFollowers);
+            // let toba = await rp(options2)
+            for(let i = 0; i < userData.length; i++){
+                myFollowers[i+1] = {username: userData[i].login}
+            }
+            // for(let i = 0; i<myFollowers.length; i++){
+            //     options2.uri = rp(`https://api.github.com/search/commits?q=author:${myfollowers[i].username}&type=Commits`)
+            //     let totalCommitCounts = await rp(options2)
+            //     totalCommitCounts = JSON.parse(totalCommitCounts).total_count;
+            //     console.log(totalCommitCounts);
+            // }
+            // console.log(allMyFollowers)
+            // res.send(myFollowers);
+            res.redirect("destination")
+            } catch (error) {
+                res.send(error)
+            }
+    }
+  
+    main()
+})
+app.get("/destination",(req,res)=>{
+    async function finalstuff(){
+        try{
+        for(let i = 0; i<myFollowers.length; i++){
+                options2.uri = rp(`https://api.github.com/search/commits?q=author:${myfollowers[i].username}&type=Commits`)
+                let totalCommitCounts = await rp(options2)
+                totalCommitCounts = JSON.parse(totalCommitCounts).total_count;
+                console.log(totalCommitCounts);
+            }
         }
         catch(error){
             res.send(error)
         }
-    })
-    
+    }
+    finalstuff()
 })
 
 app.listen(9500,()=>{
